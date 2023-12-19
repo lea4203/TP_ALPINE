45ed6251b38b3faf239011a18de51dce33ffa071
lea4203 <lea.merley@orange.fr>
2023-12-19 13:46:51 +0100

    ajout des couleurs sur les alpines

--- src/Components/Carrousel.js
+++ src/Components/Carrousel.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectVersion } from '../store/rootReducer';
 import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { selectVersion, changeColor } from '../store/rootReducer';
+import './Carrousel.css';
 
 // Importez les images individuellement ici
 import AlpinePureImage from '../images/sources-homepage/versions/ALPINE-PURE-1.png';
 import AlpineLegendeImage from '../images/sources-homepage/versions/ALPINE-LEGENDE-1.png';
+import BlueColorImage from '../images/configurateur/couleurs/selection/bleu.jpg';
+import BlackColorImage from '../images/configurateur/couleurs/selection/noir.jpg';
+import WhiteColorImage from '../images/configurateur/couleurs/selection/blanc.jpg';
+
 
 const Carrousel = () => {
   const versions = useSelector((state) => state.carrousel.versions);
   const selectedVersion = useSelector((state) => state.carrousel.selectedVersion);
-  const dispatch = useDispatch();
 
+  const dispatch = useDispatch();
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleSlideSelect = (selectedIndex) => {
@@ -20,22 +24,54 @@ const Carrousel = () => {
     dispatch(selectVersion(versions[selectedIndex].id));
   };
 
+  const handleChangeColor = (color) => {
+    dispatch(changeColor(color));
+  };
+
   return (
-    <div>
+    <div className="carrousel-container">
       <Carousel activeIndex={currentSlide} onSelect={handleSlideSelect}>
         {versions.map((version, index) => (
           <Carousel.Item key={version.id}>
             {/* Utilisez l'image importée correspondante pour chaque version */}
             <img
-              className="d-block w-100"
+              className="d-block w-50 mx-auto carrousel-image"
               src={version.id === 1 ? AlpinePureImage : AlpineLegendeImage}
-              alt={version.name}
+              alt=""
               onClick={() => dispatch(selectVersion(version.id))}
             />
-            <Carousel.Caption>
+            <div className="carrousel-info">
               <h3>{version.name}</h3>
-              <p>{version.description}</p>
-            </Carousel.Caption>
+              <p>Couleur sélectionnée : {selectedVersion.color}</p>
+              <p>Prix : {version.price}</p>
+            </div>
+            <div className="color-container">
+              {/* Utilisez l'image importée correspondante pour chaque couleur */}
+              {version.couleurs.map((colorOption) => (
+                <div key={colorOption.name} className="color-option">
+                  <img
+                   src={
+                    colorOption.name === "Teinte spéciale Bleu Alpine"
+                      ? BlueColorImage
+                      : colorOption.name === "Teinte métallisée Noir Profond"
+                      ? BlackColorImage
+                      : colorOption.name === "Peinture opaque Blanc Glacier"
+                      ? WhiteColorImage 
+                      : null
+                  }
+                    alt={colorOption.name}
+                    className="color-thumbnail"
+                    onClick={() => handleChangeColor(colorOption.name)}
+                  />
+                  <button
+                    onClick={() => handleChangeColor(colorOption.name)}
+                    disabled={selectedVersion.color === colorOption.name}
+                  >
+                    {colorOption.name}
+                  </button>
+                </div>
+              ))}
+            </div>
           </Carousel.Item>
         ))}
       </Carousel>
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,35 +1,53 @@
 // rootReducer.js
 import { createSlice } from '@reduxjs/toolkit';
 
-
 const initialState = {
-    versions: [
-      {
-        id: 1,
-        name: "Alpine Pure",
-        price: 58000,
-        image: "/images/sources-homepage/versions/ALPINE-PURE-1.png"
-
-      },
-      {
-        id: 2,
-        name: "Alpine Légende",
-        price: 61000,
-        image: "/images/sources-homepage/versions/ALPINE-LEGENDE-1.png",      },
-    ],
-    selectedVersion: null,
-  };
-  
+  versions: [
+    {
+      id: 1,
+      name: "Alpine Pure",
+      price: 54700,
+      image: "/images/sources-homepage/versions/ALPINE-PURE-1.png",
+      couleurs: [
+        { name: "Teinte spéciale Bleu Alpine", price: 1800 },
+        { name: "Teinte métallisée Noir Profond", price: 840 },
+        { name: "Peinture opaque Blanc Glacier", price: 0 },
+      ]
+    },
+    {
+      id: 2,
+      name: "Alpine Légende",
+      price: 58500,
+      image: "/images/sources-homepage/versions/ALPINE-LEGENDE-1.png",
+      couleurs: [
+        { name: "Teinte spéciale Bleu Alpine", price: 1800 },
+        { name: "Teinte métallisée Noir Profond", price: 840 },
+        { name: "Peinture opaque Blanc Glacier", price: 0 },
+      ]
+    },
+  ],
+  selectedVersion: {
+    id: null,
+    color: 'Blue',
+  },
+};
 
 const carrouselSlice = createSlice({
-  name: 'carrousel',
-  initialState,
-  reducers: {
-    selectVersion: (state, action) => {
-      state.selectedVersion = action.payload;
+    name: 'carrousel',
+    initialState,
+    reducers: {
+      selectVersion: (state, action) => {
+        const selectedVersion = { ...action.payload, color: 'Bleu' }; // Assurez-vous de copier l'objet et d'initialiser la couleur
+        state.selectedVersion = selectedVersion;
+      },
+      changeColor: (state, action) => {
+        state.selectedVersion.color = action.payload;
+      },
     },
-  },
-});
+  });
+  
+  
 
-export const { selectVersion } = carrouselSlice.actions;
+
+export const { selectVersion , changeColor} = carrouselSlice.actions;
 export default carrouselSlice.reducer;
